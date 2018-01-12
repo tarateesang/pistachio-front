6889ee2685265fe43ad5fa483331eb7539bcf42b
tarateesang <tara.tsang.339@gmail.com>
2018-01-11 20:55:25 -0500

    push

---
+++ js/pistachio.js
@@ -0,0 +1,35 @@
+// create module
+var app = angular.module('pistachio', ['ngRoute']);
+//configure for routing
+// taratsang / -> template home.html and controller mainController
+// taratsang /about -> template about.html and controller mainController
+// taratsang /portfolio -> template portfolio.html and controller mainController
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    //route for the home page
+        .when('/home', {
+            templateUrl: 'header.html',
+            controller: 'mainController',
+            title: 'Giza.'
+        })
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'header2.html',
+            controller: 'mainController',
+            title: 'China'
+        }).otherwise({
+            redirectTo: '/home'
+        })
+}]);
+//mainController holds the icons and links of outsourced projects
+app.controller('mainController', function($scope) {
+    $scope.message = "hello world";
+});
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+        $rootScope.title = current.$$route.title;
+        // $rootScope.selected = current.$$route.selected;
+        // $rootScope.selected1 = current.$$route.selected1;
+        // $rootScope.selected2 = current.$$route.selected2;
+    });
+}]);
